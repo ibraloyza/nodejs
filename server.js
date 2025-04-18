08e9c866a235448dccdc8cebeeb97f552b7bb80d
ibraloyza <ibraahim3523@gmail.com>
2025-04-18 21:09:45 +0300

    simple routing

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
-import http, { createServer } from 'http';
-const PROT = 5000;
+import { log } from 'console';
+import {createServer } from 'http'
+const PORT = process.env.PORT ;
+// const pass = process.env.pass;
+// console.log(pass);
 
-const server = createServer((req,res) =>{
-    res.setHeader( 'content-type', 'text/html');
-    res.statusCode = 404;
 
-    // res.writeHead( 'content-type', 'text/html');
-    res.end(`<h1>hello welcome!</h1>`);
-})
+const server = createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.end(`<h1>This is Home page!</h1>`);   
+    }
+    else if(req.url === '/About'){
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.end(`<h1>This is About page!</h1>`);
+    }
+    else{
+        res.writeHead(404, { 'content-type': 'text/html' });
+        res.end(`<h1>your request page is no found!</h1>`);
+    }
 
-server.listen(PROT,(req,res) =>{
-    console.log(`server runing on port ${PROT}`);
+
+
+
     
-})
+    
+});
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
