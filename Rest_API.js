ae7a0ee15a5115c1c7102f7687701cce7e36b9d1
ibraloyza <ibraahim3523@gmail.com>
2025-04-19 07:54:09 +0300

    add logger middleware

--- Rest_API.js
+++ Rest_API.js
@@ -1,6 +1,12 @@
 import { createServer } from 'http';
 const PORT = process.env.PORT;
 
+// logger middleware
+const logger = (req, res, next) =>{
+    console.log(`${req.method} ,${req.url}`);
+    next();    
+}
+
 const  users =[
     {
         id:1,
@@ -36,33 +42,36 @@ const  users =[
 
 
 const server = createServer((req, res) =>{
-    if (req.url === '/api/users' && req.method === 'GET') {
-         res.setHeader('content-type','application/json');
-         res.write(JSON.stringify(users));
-         res.end();
-         
-    }
-    else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET' ) 
-    {
-        const id = req.url.split('/')[3];
-        const user = users.find((user) => user.id === parseInt(id))
-        res.setHeader('content-type','application/json');
+    logger(req, res,() =>{
+        if (req.url === '/api/users' && req.method === 'GET') {
+            res.setHeader('content-type','application/json');
+            res.write(JSON.stringify(users));
+            res.end();
+            
+       }
+       else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET' ) 
+       {
+           const id = req.url.split('/')[3];
+           const user = users.find((user) => user.id === parseInt(id))
+           res.setHeader('content-type','application/json');
+   
+           if (user) {
+               res.write(JSON.stringify(user));
+           }else{
+               res.statusCode = 404;
+               res.write(JSON.stringify({massage: 'user not found'}));
+           }
+           res.end();              
+       }
+       else
+       {
+           res.setHeader('content-type','application/json');
+           res.statusCode = 404;
+           res.write(JSON.stringify({massage: 'Rout not found'}));
+           res.end();
+       }
+    });
 
-        if (user) {
-            res.write(JSON.stringify(user));
-        }else{
-            res.statusCode = 404;
-            res.write(JSON.stringify({massage: 'user not found'}));
-        }
-        res.end();              
-    }
-    else
-    {
-        res.setHeader('content-type','application/json');
-        res.statusCode = 404;
-        res.write(JSON.stringify({massage: 'Rout not found'}));
-        res.end();
-    }
 })
 
 
