15d14e64fdc8eb1c315d709f8cff3c13b6713880
ibraloyza <ibraahim3523@gmail.com>
2025-04-20 14:46:45 +0300

    add readFile() -promise .then()

--- fsDemo.js
+++ fsDemo.js
@@ -1,13 +1,21 @@
-import fs from 'fs';
+// import fs from 'fs';
+
+import fs from 'fs/promises';
 
 // readFile() - callback ver
 
-fs.readFile('./test.txt', 'utf8',(err,data) =>{
-    if(err) throw err;
-    console.log(data);  
-});
+// fs.readFile('./test.txt', 'utf8',(err,data) =>{
+//     if(err) throw err;
+//     console.log(data);  
+// });
 
 // readFileSync() - syncronous ver
 
-const data = fs.readFileSync('./test.txt','utf8');
-console.log(data);
+// const data = fs.readFileSync('./test.txt','utf8');
+// console.log(data);
+
+//readFile() -promise .then()
+fs.readFile('./test.txt','utf8')
+.then((data) => console.log(data))
+.catch((err) =>console.log(err));
+
